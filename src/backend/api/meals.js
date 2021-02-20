5f8d830934f54a90b04a0afe476002a5ea0e5d39
Beth Jackson <beth.jack@gmail.com>
2021-02-20 16:22:30 +0100

    Finished GET api/meals/ query parameters

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -4,10 +4,47 @@ const knex = require("../database");
 const getMeal = require("../modules/getMeal");
 
 router.get("/", async (request, response) => {
+  const maxPrice =
+    request.query.maxPrice === undefined
+      ? undefined
+      : Number(request.query.maxPrice);
+  const availableReservations = Boolean(request.query.availableReservations);
+  const title = request.query.title;
+  const createdAfter =
+    request.query.createdAfter === undefined
+      ? undefined
+      : new Date(request.query.createdAfter);
+  const limit =
+    request.query.limit === undefined ? undefined : Number(request.query.limit);
+
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meal").select("*");
-    response.json(titles);
+    // const titles = await knex("meals").select("title");
+    // response.json(titles);
+
+    let dbQuery = knex("meal");
+    if (typeof maxPrice === "number") {
+      dbQuery = dbQuery.where("price", "<=", maxPrice);
+    }
+
+    if (availableReservations) {
+      dbQuery = dbQuery.whereRaw(`meal.max_reservations > (
+      SELECT SUM(reservation.number_of_guests)
+      FROM reservation
+      WHERE reservation.meal_id = meal.id
+      )`);
+    }
+
+    if (typeof title === "string") {
+      dbQuery = dbQuery.where("title", "like", `%${title}%`);
+    }
+    if (createdAfter !== undefined) {
+      dbQuery = dbQuery.where("created_date", ">", createdAfter);
+    }
+    if (typeof limit === "number") {
+      dbQuery = dbQuery.limit(limit);
+    }
+    const meals = await dbQuery;
+    return response.json(meals);
   } catch (error) {
     throw error;
   }
